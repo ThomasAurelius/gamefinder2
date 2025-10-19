6e37d9564714bc82d0f05995956ccdc119989126
Thomas Gooch <teg824@hotmail.com>
2025-10-18 21:52:00 -0500

    update

--- lib/ambassador-config.ts
+++ lib/ambassador-config.ts
@@ -3,7 +3,7 @@
  */
 
 // Ambassador program expiration date: June 30, 2026 at 23:59:59 UTC
-export const AMBASSADOR_EXPIRATION_DATE = new Date('2026-06-30T23:59:59Z');
+export const AMBASSADOR_EXPIRATION_DATE = new Date("2026-03-31T23:59:59Z");
 
 // Human-readable expiration date for display
-export const AMBASSADOR_EXPIRATION_DISPLAY = 'June 30th, 2026';
+export const AMBASSADOR_EXPIRATION_DISPLAY = "March 31st, 2026";
